8d937814e13c65da3a8255432fc9d883a2a1482a
Vivek Sharma <viveksharma02005@gmail.com>
2026-01-11 00:39:54 +0530

    Feat:done by using google  gemini

--- GenAi/02hello_world/main.py
+++ GenAi/02hello_world/main.py
@@ -12,3 +12,22 @@ response = clint.chat.completions.create(
 )
 
 print(response.choices[0].message.content)
+
+
+
+##By using Google Gemini API Key
+# from openai import OpenAI
+# clint = OpenAI(
+#     api_key="YOUR_GOOGLE_GEMINI_API_KEY",
+#     base_url="https://generativelanguage.googleapis.com/v1beta/"
+    
+# )
+
+# response = clint.chat.completions.create(
+#     model="gemini-2.5-flash",
+#     messages=[
+#         {"role": "user", "content":"Hey  there!"}
+#     ]
+# )
+
+# print(response.choices[0].message.content)
